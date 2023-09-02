227d6d0f9c88efad0788701bdbc620fb056dc397
LiziAC05 <lizbeth.aquisse@pucp.edu.pe>
2023-09-02 09:17:40 -0500

    hola

--- app/src/main/java/com/example/lab1_20181650/MainActivity.java
+++ app/src/main/java/com/example/lab1_20181650/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.lab1_20181650;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Menu;
 import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
@@ -13,5 +14,8 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
-
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main_act,menu);
+        return true;
+    }
 }
