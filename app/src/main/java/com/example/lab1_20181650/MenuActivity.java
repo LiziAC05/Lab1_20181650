e5e9b8117b6278432eb53a328f9acee228fed42f
LiziAC05 <lizbeth.aquisse@pucp.edu.pe>
2023-09-01 21:51:49 -0500

    hola

--- app/src/main/java/com/example/lab1_20181650/MenuActivity.java
+++ app/src/main/java/com/example/lab1_20181650/MenuActivity.java
@@ -1,5 +1,6 @@
 package com.example.lab1_20181650;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.annotation.SuppressLint;
@@ -7,7 +8,9 @@ import android.content.Context;
 import android.os.Bundle;
 import android.view.ContextMenu;
 import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.TextView;
 
 import com.example.lab1_20181650.databinding.ActivityMenuBinding;
 
@@ -19,6 +22,12 @@ public class MenuActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         binding = ActivityMenuBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
+        registerForContextMenu((TextView) findViewById(R.id.textView2));
+    }
+
+    public void onContextItemSelect(@NonNull MenuItem item){
+        super.onContextItemSelected(item);
+
     }
 
 }
