83a205775078954cb786bec78e6f130e8644740e
LiziAC05 <lizbeth.aquisse@pucp.edu.pe>
2023-09-02 12:56:31 -0500

    hola

--- app/src/main/java/com/example/lab1_20181650/MainActivity.java
+++ app/src/main/java/com/example/lab1_20181650/MainActivity.java
@@ -1,10 +1,12 @@
 package com.example.lab1_20181650;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
@@ -15,20 +17,24 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
     }
-
+    @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.menu_main_act,menu);
         return true;
     }
 
-    public void returnBtn(){
-        Intent intent = new Intent(MainActivity.this, MenuActivity.class);
-        startActivity(intent);
-        //finish();
-    }
-
-    public void statBtn(){
+    /*public void returnBtn(){
         Intent intent = new Intent(MainActivity.this, StatsActivity.class);
         startActivity(intent);
+        //finish();
+    }*/
+
+    public boolean onOptionsItemSelected(@NonNull MenuItem item){
+        if(item.getItemId() == R.id.stadistics){
+            Intent intent = new Intent(MainActivity.this, StatsActivity.class);
+            startActivity(intent);
+        }
+        return super.onOptionsItemSelected(item);
     }
+
 }
